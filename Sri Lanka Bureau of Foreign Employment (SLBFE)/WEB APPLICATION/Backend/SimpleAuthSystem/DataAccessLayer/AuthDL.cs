a5f5ad6eb99b17f5544391cf4ee98dbf0410fb25
Neranji Sulakshika <neranjisulakshika@gmail.com>
2022-04-21 23:01:43 +0530

    Web Application - Backend & Frontend (SignIn & SignUp)

---
+++ Sri Lanka Bureau of Foreign Employment (SLBFE)/WEB APPLICATION/Backend/SimpleAuthSystem/DataAccessLayer/AuthDL.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using SimpleAuthSystem.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleAuthSystem.DataAccessLayer
+{
+    public class AuthDL : IAuthDL
+    {
+        public readonly IConfiguration _configuration;
+        public readonly MySqlConnection _mySqlConnection;
+
+        public AuthDL(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _mySqlConnection = new MySqlConnection(_configuration["ConnectionStrings:MySqlDBConnectionString"]);
+        }
+
+        public async Task<SignInResponse> SignIn(SignInRequest request)
+        {
+            SignInResponse response = new SignInResponse();
+            response.IsSuccess = true;
+            response.Message = "Successful";
+            try
+            {
+
+                if(_mySqlConnection.State != System.Data.ConnectionState.Open)
+                {
+                    await _mySqlConnection.OpenAsync();
+                }
+
+                string SqlQuery = @"SELECT * 
+                                    FROM slbfe_citizens.citizen_details 
+                                    WHERE Email=@Email AND Password=@PassWord AND Affiliation=@Affiliation;";
+
+                using (MySqlCommand sqlCommand = new MySqlCommand(SqlQuery, _mySqlConnection))
+                {
+                    sqlCommand.CommandType = System.Data.CommandType.Text;
+                    sqlCommand.CommandTimeout = 180;
+                    sqlCommand.Parameters.AddWithValue("@Email", request.Email);
+                    sqlCommand.Parameters.AddWithValue("@PassWord", request.Password);
+                    sqlCommand.Parameters.AddWithValue("@Affiliation", request.Affiliation);
+                    using (DbDataReader dataReader = await sqlCommand.ExecuteReaderAsync())
+                    {
+                        if (dataReader.HasRows)
+                        {
+                            response.Message = "Login Successfull";
+                        }
+                        else
+                        {
+                            response.IsSuccess = false;
+                            response.Message = "Login Failed";
+                            return response;
+                        }
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.Message;
+            }
+            finally
+            {
+
+            }
+
+            return response;
+        }
+
+        public async Task<SignUpResponse> SignUp(SignUpRequest request)
+        {
+            SignUpResponse response = new SignUpResponse();
+            response.IsSuccess = true;
+            response.Message = "Successful";
+            try
+            {
+                if (_mySqlConnection.State != System.Data.ConnectionState.Open)
+                {
+                    await _mySqlConnection.OpenAsync();
+                }
+
+                if (!request.Password.Equals(request.ConfigPassword))
+                {
+                    response.IsSuccess = false;
+                    response.Message = "Password & Confirm Password not Match";
+                    return response;
+                }
+
+                string SqlQuery = @"INSERT INTO 
+                                    slbfe_citizens.citizen_details 
+                                    (NIC, Name, Address, Age, Profession, Email, PassWord, Affiliation) VALUES 
+                                    (@NIC, @Name, @Address, @Age, @Profession, @Email, @PassWord, @Affiliation)";
+
+                using (MySqlCommand sqlCommand = new MySqlCommand(SqlQuery, _mySqlConnection))
+                {
+                    sqlCommand.CommandType = System.Data.CommandType.Text;
+                    sqlCommand.CommandTimeout = 180;
+                    sqlCommand.Parameters.AddWithValue("@NIC", request.NIC);
+                    sqlCommand.Parameters.AddWithValue("@Name", request.Name);
+                    sqlCommand.Parameters.AddWithValue("@Address", request.Address);
+                    sqlCommand.Parameters.AddWithValue("@Age", request.Age);
+                    sqlCommand.Parameters.AddWithValue("@Profession", request.Profession);
+                    sqlCommand.Parameters.AddWithValue("@Email", request.Email);
+                    sqlCommand.Parameters.AddWithValue("@PassWord", request.Password);
+                    sqlCommand.Parameters.AddWithValue("@Affiliation", request.Affiliation);
+                    int Status = await sqlCommand.ExecuteNonQueryAsync();
+                    if(Status <= 0)
+                    {
+                        response.IsSuccess = false;
+                        response.Message = "Something Went Wrong";
+                        return response;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.Message;
+            }
+            finally
+            {
+
+            }
+
+            return response;
+        }
+    }
+}
