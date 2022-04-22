115cc75c14fabe5b8da7e60db1d9585c0701ec7d
Neranji Sulakshika <neranjisulakshika@gmail.com>
2022-04-22 12:32:47 +0530

    Backend Changes

---
+++ Sri Lanka Bureau of Foreign Employment (SLBFE)/WEB APPLICATION/Backend/Backend/Model/SignIn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Model
+{
+    public class SignInRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Affiliation { get; set; }
+    }
+
+    public class SignInResponse
+    { 
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+    }
+}
