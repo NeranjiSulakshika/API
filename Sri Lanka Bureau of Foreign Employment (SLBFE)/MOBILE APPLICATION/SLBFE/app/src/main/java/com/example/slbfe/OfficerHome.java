038b04b38bd13e0e9ef45cc36b3a0bb222b09bf7
Samadhi <samadhikaushalya000@gmail.com>
2022-05-11 15:21:34 +0530

    mobile app

---
+++ Sri Lanka Bureau of Foreign Employment (SLBFE)/MOBILE APPLICATION/SLBFE/app/src/main/java/com/example/slbfe/OfficerHome.java
@@ -0,0 +1,14 @@
+package com.example.slbfe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OfficerHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_officer_home);
+    }
+}
